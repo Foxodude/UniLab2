6a151ce16e2ded71d03c10396dc137d735195b30
Foxodude <apolmaks1@gmail.com>
2024-03-05 18:58:54 +0300

    1Задание

--- UniLab2.py
+++ UniLab2.py
@@ -1,1 +1,8 @@
-
+# -*- coding: cp1251 -*-
+passw = input("Создайте пароль : ")
+print("Чтобы продолжить вам нужно ввести пароль заново")
+passwConfirm = input("Введите пароль : ")
+if passwConfirm == passw :
+    print("Пароль принят")
+else :
+    print("Пароль отклонен")
